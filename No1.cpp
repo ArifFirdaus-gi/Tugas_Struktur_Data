ccc200ad7b7916ad754fd28dcf089e6007602987
ArifFirdaus-gi <arif170107firdaus@gmail.com>
2026-05-01 22:39:56 +0700

    No1.cpp

--- No1.cpp
+++ No1.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
 
-//single linked list
 struct Node
 {
 	int data;
 	struct Node *next;
+	struct Node *prev;
 };
 
-void transerver(struct Node *temp);
+void asc(struct Node *nodeSekarang);
+void dec(struct Node *nodeSekarang);
 
 int main(){
 
@@ -20,34 +21,51 @@ int main(){
 	node2 = new Node;
 	node3 = new Node;
 	
+	
 	node1->data = 10;
 	node1->next = node2;
+	node1->prev = NULL;
 	
 	node2->data = 20;
 	node2->next = node3;
+	node2->prev = node1;
 	
 	node3->data = 30;
 	node3->next = NULL;
+	node3->prev = node2;
 	
-	transerver (node1);
+	cout <<"Ini Asc" <<endl;
+	asc (node1);
+	
+	
+	cout <<"Ini Prev" <<endl;
+	dec (node3);
 	
 	cin.get();
 	return 0;
 }
 
-//looping untuk menampilkan data linked list
-void transerver(struct Node *temp)
+void asc(struct Node *nodeSekarang)
 {
 	int i = 1;
 	int jumlah = 0;
-	while (temp != NULL)
-	{
-		cout << "data ke " << i << ": ";
-		cout << temp->data;
-		temp = temp->next;
-		cout << endl;
-		jumlah++;
+	while((nodeSekarang != NULL) && (i <= 3)){
+		cout << "Data ke-"<<i<<": "<<nodeSekarang->data <<endl;
+		nodeSekarang = nodeSekarang->next;
 		i++;
+		jumlah++;
 	}
-	cout << "Jumlah data: "<<jumlah;	
+	cout << "Jumlah data: " << jumlah <<endl <<endl;
+}
+
+void dec(struct Node *nodeSekarang){
+	int i = 1;
+	int jumlah = 0;
+	while((nodeSekarang != NULL) && (i <= 3)){
+		cout << "Data ke-"<<i<<": "<<nodeSekarang->data <<endl;
+		nodeSekarang = nodeSekarang->prev;
+		i++;
+		jumlah++;
+	}
+	cout << "Jumlah data: " << jumlah;
 }
